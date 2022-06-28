5daedb323118ea1d077c410cfb01381147a54c6c
EdersonRodrigues7 <ederson@trilhante.com.br>
2022-06-28 14:43:00 -0300

    chore: implements getLast method

--- src/Models/PrismaModels/PrismaTransaction.ts
+++ src/Models/PrismaModels/PrismaTransaction.ts
@@ -1,6 +1,6 @@
+import { type } from 'os';
 import { prisma } from '../../prisma';
 import { TransactionModel, TransactionData } from '../Transaction';
-
 export class PrismaTransactionModel implements TransactionModel {
   async create({ title, type, category, amount, createdAt }: TransactionData) {
     await prisma.transaction.create({
@@ -16,5 +16,30 @@ export class PrismaTransactionModel implements TransactionModel {
   async show() {
     const allTransactions = await prisma.transaction.findMany();
     return allTransactions;
+  }
+  async getLast() {
+    const transactionsCount = await prisma.transaction.count({
+      select: {
+        _all: true
+      }
+    });
+    const query = await prisma.transaction.findUnique({
+      where: {
+        id: transactionsCount._all
+      }
+    });
+    if (query) {
+      const lastTransaction: TransactionData = {
+        id: query?.id,
+        title: query.title,
+        type: query.type,
+        category: query.category,
+        amount: query.amount,
+        createdAt: query.createdAt
+      };
+      return lastTransaction;
+    } else {
+      return undefined;
+    }
   }
 }
